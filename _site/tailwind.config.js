d2db9451ac12107736f3c92cf6575eecb4fb6b34
a-chacon <andres.ch@protonmail.com>
2022-04-25 18:40:28 -0400

    feat: add site

---
+++ _site/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    '_site/**/*.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Monaco', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
