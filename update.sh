4c9bd0e8357f15979a4eeb7b18694f89b96b58aa
Andres Chacón <andres.ch@protonmail.com>
2022-09-04 01:08:29 -0400

    run: update dom 04 sep 2022 01:08:29

--- update.sh
+++ update.sh
@@ -1,7 +1,11 @@
+#!/usr/bin/env bash
+
+#here should update somthing
 
 bundle exec jekyll build
 
 git add .
 timestamp=$(date +%c)
-git commit -m "feat: update $timestamp"
+git commit -m "run: update $timestamp"
+
 git push origin main
