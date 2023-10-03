9cb8a9131c915281adecd4b7753a384f44dbc938
a-chacon <andres.ch@protonmail.com>
2023-10-03 20:23:13 +0200

    refactor: remove site folder

--- _site/en/deploy.sh
+++
@@ -1,6 +0,0 @@
-sudo docker container stop a-chacon
-sudo docker container rm a-chacon
-sudo docker image rm a-chacon
-sudo docker build -t a-chacon .
-sudo docker run -d --restart always --name a-chacon -p 5000:4000 a-chacon
-
